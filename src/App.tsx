14caaf0c575d00fdf44dd096f9f4a44087007baf
kisara579 <kisarabeddawala@gmail.com>
2026-01-03 18:48:21 +0530

    feat: enhance introduction section in App component with additional details

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,15 @@ function App() {
     <>
       <div className="min-h-screen bg-black">
         <div className="flex sm:flex-row flex-col justify-center  items-center min-h-screen  ">
-          <div className="w-full  sm:w-6/12 text-white text-lg   ">Hi, I’m Kisara Beddawala</div>
+          <div className="w-full  sm:w-6/12 text-white   p-10  text-center sm:text-left sm:p-5 ">
+            <div className="text-2xl">
+              <span className="text-lg">Hi, I’m</span> Kisara Beddawala
+            </div>
+            <div className="text-lg">A computer science undergraduate</div>
+            <div className="text-md">
+              Passionate about cybersecurity, ethical hacking.
+            </div>
+          </div>
           <div className="w-full  sm:w-6/12  "></div>
         </div>
       </div>
