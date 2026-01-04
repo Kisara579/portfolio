9f33fccccf67f49b53e031eca3c81c8d9d9e92b6
kisara579 <kisarabeddawala@gmail.com>
2026-01-04 12:50:50 +0530

    Refactor code structure for improved readability and maintainability

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,25 +1,38 @@
+import UserImage from "../../assets/user.jpg";
+
 const Hero = () => {
   return (
     <>
       <div className="flex sm:flex-row flex-col justify-center  items-center min-h-screen  ">
         <div className="w-full  sm:w-6/12 text-white   p-10  text-center sm:text-left sm:p-5 ">
-          <div className="text-2xl">
-            <span className="text-lg">Hi, I’m</span> Kisara Beddawala
+          <div className="text-3xl sm:text-4xl font-mono font-bold">
+            <span className="text-xl ">Hi, I’m</span>
+            <span className="text-cyan-400"> Kisara Beddawala</span>
           </div>
-          <div className="text-lg">A computer science undergraduate</div>
-          <div className="text-md">
+          <div className="text-md sm:text-lg mt-3 font-semibold">
+            A computer science undergraduate
+          </div>
+          <div className="text-sm sm:text-md mt-2">
             Passionate about cybersecurity, ethical hacking.
           </div>
           <div className="mt-5 flex justify-center sm:justify-start">
-            <button className="text-lg">Explore My Work</button>
+            <button className="text-md sm:text-lg">Explore My Work</button>
           </div>
           <div className="mt-3 flex justify-center sm:justify-start">
-            <a href="#View-Resume" className="text-md">
+            <a href="#View-Resume" className="text-md sm:text-lg">
               View Resume
             </a>
           </div>
         </div>
-        <div className="w-full  sm:w-6/12  "></div>
+        <div className="w-full  sm:w-6/12 p-10 sm:p-5 m-10 border-2 border-cyan-400 rounded-lg">
+          <div className="flex items-center justify-center min-h-[300px] text-white">
+            <img
+              src={UserImage}
+              alt=" grid"
+              className="w-full h-auto object-contain rounded-lg"
+            />
+          </div>
+        </div>
       </div>
     </>
   );
