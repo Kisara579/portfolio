23c09a33c40974b2892b0c69909f97d98ebbf3eb
kisara579 <kisarabeddawala@gmail.com>
2026-01-04 09:10:27 +0530

    feat: implement Hero component with personal introduction and call to action

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,7 @@ function App() {
   return (
     <>
       <div className="min-h-screen bg-black">
-        <div className="flex sm:flex-row flex-col justify-center  items-center min-h-screen  ">
-          <div className="w-full  sm:w-6/12 text-white   p-10  text-center sm:text-left sm:p-5 ">
-            <div className="text-2xl">
-              <span className="text-lg">Hi, I’m</span> Kisara Beddawala
-            </div>
-            <div className="text-lg">A computer science undergraduate</div>
-            <div className="text-md">
-              Passionate about cybersecurity, ethical hacking.
-            </div>
-          </div>
-          <div className="w-full  sm:w-6/12  "></div>
-        </div>
+        <Hero />
       </div>
     </>
   );
