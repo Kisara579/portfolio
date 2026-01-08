7b7a187c4fd93ee8f1eec9fba52ea4d4f541f161
kisara579 <kisarabeddawala@gmail.com>
2026-01-08 06:16:11 +0530

    feat: enhance Projects component with hover effects for project cards

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -9,23 +9,23 @@ function Projects() {
           Some of the projects I've worked on to build my skills and experience.
         </p>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
-          <div className="bg-slate-800/50 border border-slate-700/50 rounded-lg p-6 ">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6  ">
+          <div className="bg-slate-800/50 border border-slate-700/50 rounded-lg p-6 hover:-translate-y-0.5  transition-all duration-500">
             <h3 className="text-xl font-semibold text-white mb-3">Project 1</h3>
           </div>
-          <div className="bg-slate-800/50 border border-slate-700/50 rounded-lg p-6 ">
+          <div className="bg-slate-800/50 border border-slate-700/50 rounded-lg p-6 hover:-translate-y-0.5  transition-all duration-500">
             <h3 className="text-xl font-semibold text-white mb-3">Project 2</h3>
           </div>
-          <div className="bg-slate-800/50 border border-slate-700/50 rounded-lg p-6 ">
+          <div className="bg-slate-800/50 border border-slate-700/50 rounded-lg p-6 hover:-translate-y-0.5  transition-all duration-500">
             <h3 className="text-xl font-semibold text-white mb-3">Project 3</h3>
           </div>
-          <div className="bg-slate-800/50 border border-slate-700/50 rounded-lg p-6 ">
+          <div className="bg-slate-800/50 border border-slate-700/50 rounded-lg p-6 hover:-translate-y-0.5  transition-all duration-500">
             <h3 className="text-xl font-semibold text-white mb-3">Project 4</h3>
           </div>
-          <div className="bg-slate-800/50 border border-slate-700/50 rounded-lg p-6 ">
+          <div className="bg-slate-800/50 border border-slate-700/50 rounded-lg p-6 hover:-translate-y-0.5  transition-all duration-500">
             <h3 className="text-xl font-semibold text-white mb-3">Project 5</h3>
           </div>
-          <div className="bg-slate-800/50 border border-slate-700/50 rounded-lg p-6 ">
+          <div className="bg-slate-800/50 border border-slate-700/50 rounded-lg p-6 hover:-translate-y-0.5  transition-all duration-500">
             <h3 className="text-xl font-semibold text-white mb-3">Project 6</h3>
           </div>
         </div>
